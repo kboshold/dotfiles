e2d8e1e6f3413ce2e3cc231cace4a18cf91333af
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-05 19:11:04 +0000

    Added configs

---
+++ dot_config/nvim/lua/kpalatzky/init.lua
@@ -0,0 +1,17 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Load package manager
+require('kpalatzky.lazy')
+
+-- Load plugins
+require('kpalatzky.plugins')
+
+-- Set keybindings
+require('kpalatzky.remap')
+
+-- Set options
+require('kpalatzky.set')
