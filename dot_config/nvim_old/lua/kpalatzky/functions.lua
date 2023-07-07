0d678db14ac3b45a46ee9bfff89f8d23ab48585c
Kevin Boshold <github@proxy.boshol.de>
2023-07-07 16:30:31 +0200

    Removed nvim old

--- dot_config/nvim_old/lua/kpalatzky/functions.lua
+++
@@ -1,7 +0,0 @@
-set_key = function(mode, keys, func, desc, opts, context)
-    if not opts then
-        opts = {}
-    end
-    opts.desc = desc
-    vim.keymap.set(mode, keys, func, opts)
-end
--- dot_config/nvim_old/lua/kpalatzky/remap.lua
+++
@@ -1,8 +0,0 @@
-
-set_key("n", "<leader>pv", vim.cmd.Ex, "Open [p]arent [v]iew")
-
--- Diagnostic keymaps
-set_key('n', '[d', vim.diagnostic.goto_prev, 'Go to previous [d]iagnostic message')
-set_key('n', ']d', vim.diagnostic.goto_next, 'Go to next [d]iagnostic message')
-set_key('n', '<leader>e', vim.diagnostic.open_float, 'Open floating diagnostic message')
-set_key('n', '<leader>q', vim.diagnostic.setloclist, 'Open diagnostics list')
