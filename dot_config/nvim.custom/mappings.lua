7a2b05eef313b78ce7adea6570a69b138fde1c5c
Kevin Boshold <github@proxy.boshol.de>
2023-07-07 22:45:05 +0200

    Update mappings.lua

--- dot_config/nvim.custom/mappings.lua
+++ dot_config/nvim.custom/mappings.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.general = {
-  n = {
+  v = {
     ["<leader>te"] = { function() require("translate").translate("EN", {"-command=deepl_free"}) end, "Translate selection to englisch", { nowait = true}},
     ["<leader>tg"] = { function() require("translate").translate("DE", {"-command=deepl_free"}) end, "Translate selection to german", { nowait = true}}
   },
