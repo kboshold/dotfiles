b10cc4caba3ff7b20d0fb115e8de17898a2be3df
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-07 22:43:53 +0200

    Update mappings.lua

--- dot_config/nvim.custom/mappings.lua
+++ dot_config/nvim.custom/mappings.lua
@@ -3,8 +3,8 @@ local M = {}
 
 M.general = {
   n = {
-    ["<leader>te"] = { function() require("translate").translate("EN", ["-command=deepl_free"]) end, "Translate selection to englisch", { nowait = true}},
-    ["<leader>tg"] = { function() require("translate").translate("DE", ["-command=deepl_free"]) end, "Translate selection to german", { nowait = true}}
+    ["<leader>te"] = { function() require("translate").translate("EN", {"-command=deepl_free"}) end, "Translate selection to englisch", { nowait = true}},
+    ["<leader>tg"] = { function() require("translate").translate("DE", {"-command=deepl_free"}) end, "Translate selection to german", { nowait = true}}
   },
 }
 
