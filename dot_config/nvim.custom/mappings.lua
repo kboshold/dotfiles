03210114d00056339848b94830548797a5d8b100
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-07 22:49:03 +0200

    Update mappings.lua

--- dot_config/nvim.custom/mappings.lua
+++ dot_config/nvim.custom/mappings.lua
@@ -3,8 +3,8 @@ local M = {}
 
 M.general = {
   v = {
-    ["<leader>te"] = { function() require("translate").translate("EN", {"-command=deepl_free"}) end, "Translate selection to englisch", { nowait = true}},
-    ["<leader>tg"] = { function() require("translate").translate("DE", {"-command=deepl_free"}) end, "Translate selection to german", { nowait = true}}
+    ["<leader>te"] = { function() require("translate").translate(vim.fn.mode() or vim.fn.visualmode(), {"EN", "-command=deepl_free"}) end, "Translate selection to englisch", { nowait = true}},
+    ["<leader>tg"] = { function() require("translate").translate(vim.fn.mode() or vim.fn.visualmode(), {"DE", "-command=deepl_free"}) end, "Translate selection to german", { nowait = true}}
   },
 }
 
