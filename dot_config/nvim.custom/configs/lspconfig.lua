a2ec91d399e017e201f4eae827901eb467bd68e5
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-10 18:23:57 +0200

    Update lspconfig.lua

--- dot_config/nvim.custom/configs/lspconfig.lua
+++ dot_config/nvim.custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "angularls", "typescript"}
+local servers = { "html", "cssls", "clangd", "angularls", "tsserver"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
