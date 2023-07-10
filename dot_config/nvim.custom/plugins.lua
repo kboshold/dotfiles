9640bfba258843111312222f169c22f8c8f58e59
Kevin Boshold <github@proxy.boshol.de>
2023-07-10 18:11:03 +0200

    Update plugins.lua

--- dot_config/nvim.custom/plugins.lua
+++ dot_config/nvim.custom/plugins.lua
@@ -23,7 +23,15 @@ local plugins = {
         },
       }
     end
-  }
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
 
 }
 
