ab6d2e42f9742e1b1f1a3604286374ce7225d357
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-10 10:56:35 +0200

    Create dot_wezterm.lua

---
+++ dot_wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font 'JetBrainsMono NL NFM'
+config.colors = {
+    background = '#262626'
+}
+config.enable_tab_bar = false
+
+
+wezterm.on("gui-startup", function()
+  local _tab, _pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
+-- and finally, return the configuration to wezterm
+return config
