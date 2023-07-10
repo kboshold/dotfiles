ed86dc4ab28e209bd21b2d1a6bea81c85878c743
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-10 19:23:46 +0200

    Update options.lua

--- dot_config/nvim.custom/options.lua
+++ dot_config/nvim.custom/options.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
