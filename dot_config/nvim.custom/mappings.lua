c9a5eb0c6d239a70867816fe8114776c4096ec41
Kevin Boshold <github@proxy.boshol.de>
2023-07-11 15:36:45 +0200

    Update mappings.lua

--- dot_config/nvim.custom/mappings.lua
+++ dot_config/nvim.custom/mappings.lua
@@ -4,7 +4,8 @@ local M = {}
 M.general = {
   n = {
     ["<leader>te"] = { "<CMD>Translate EN<CR>", "Translate selection to englisch", { nowait = true}},
-    ["<leader>tg"] = { "<CMD>Translate DE<CR>", "Translate selection to german", { nowait = true}}
+    ["<leader>tg"] = { "<CMD>Translate DE<CR>", "Translate selection to german", { nowait = true}},
+    ["<leader>gg"] = { "<CMD>LazyGit<CR>", "Open LazyGit", { nowait = true }}
   },
 }
 
