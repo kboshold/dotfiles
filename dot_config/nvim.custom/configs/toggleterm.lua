db2860f2351c3805867bfcd7694d9ed2c4f3da5b
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-12 12:41:49 +0200

    Update toggleterm.lua

--- dot_config/nvim.custom/configs/toggleterm.lua
+++ dot_config/nvim.custom/configs/toggleterm.lua
@@ -10,6 +10,10 @@ local lazydocker_client = Terminal:new {
   float_opts = {
     border = "double",
   },
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
 }
 
 -- LazyGit
@@ -21,6 +25,10 @@ local lazygit_client = Terminal:new {
   float_opts = {
     border = "double",
   },
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
 }
 
 vim.api.nvim_create_user_command("LazyGitToggle", function()
