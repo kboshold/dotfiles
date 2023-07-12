981ed7bc5631a95ad9ba026d75dc1cbab191028e
Kevin Boshold <github@proxy.boshol.de>
2023-07-12 21:53:05 +0200

    Create autosave.lua

---
+++ dot_config/nvim.custom/configs/autosave.lua
@@ -0,0 +1,24 @@
+local autoSave = require "auto-save"
+
+autoSave.setup {
+  -- The name of the augroup.
+  augroup_name = "AutoSavePlug",
+  execution_message = {
+    message = function()
+      return ""
+    end,
+    dim = 0.18,
+    cleaning_interval = 1250,
+  },
+  events = { "InsertLeave", "TextChanged" },
+  silent = true,
+  save_fn = function()
+    if save_cmd ~= nil then
+      vim.cmd(save_cmd)
+    elseif silent then
+      vim.cmd "silent! w"
+    else
+      vim.cmd "w"
+    end
+  end,
+}
