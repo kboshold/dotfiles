2c54a56b7a94b29fe229e7e28987d62a9c2ef120
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-12 20:50:12 +0200

    Update biscuits.lua

--- dot_config/nvim.custom/configs/biscuits.lua
+++ dot_config/nvim.custom/configs/biscuits.lua
@@ -5,7 +5,7 @@ biscuits.setup {
   default_config = {
     min_distance = 10,
     max_length = 50,
-    prefix_string = " 󰆘 ",
+    prefix_string = "  ",
     prefix_highlight = "Comment",
     enable_linehl = true,
   },
