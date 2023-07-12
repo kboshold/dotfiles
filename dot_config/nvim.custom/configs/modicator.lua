f0034cefd0d003ffed9b872900111db456161529
Kevin Boshold <github@proxy.boshol.de>
2023-07-12 21:47:26 +0200

    Update modicator.lua

--- dot_config/nvim.custom/configs/modicator.lua
+++ dot_config/nvim.custom/configs/modicator.lua
@@ -10,26 +10,36 @@ modicator.setup {
     }
 }
 
- local function set_mode_hls_from_lualine()
-      local modes = {
-        "Normal",
+local function set_mode_hls_from_lualine()
+    vim.api.nvim_set_hl(
+        0,
+        "InsertMode",
+        {
+            fg = "#f8f8f8",
+            bold = true
+        }
+    )
+    local modes = {
         "Insert",
         "Visual",
         "Command",
         "Replace",
         "Select",
         "Terminal",
-        "TerminalNormal",
-      }
+        "TerminalNormal"
+    }
 
-      for _, mode in pairs(modes) do
-        local hl = vim.api.nvim_get_hl(0, { name = "St_" .. mode .. "Mode", link = false })
-        vim.api.nvim_set_hl(0, mode .. "Mode", { 
-                fg = hl.bg, 
-                italic = true,
-                bold = true 
-        })
-      end
+    for _, mode in pairs(modes) do
+        local hl = vim.api.nvim_get_hl(0, {name = "St_" .. mode .. "Mode", link = false})
+        vim.api.nvim_set_hl(
+            0,
+            mode .. "Mode",
+            {
+                fg = hl.bg,
+                bold = true
+            }
+        )
     end
+end
 
-    set_mode_hls_from_lualine()
+set_mode_hls_from_lualine()
