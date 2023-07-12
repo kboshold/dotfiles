4eebee821e16fb68aa7350494c77a59891670c81
Kevin Palatzky <kevin.palatzky@gmail.com>
2023-07-12 10:52:51 +0200

    Update mappings.lua

--- dot_config/nvim.custom/mappings.lua
+++ dot_config/nvim.custom/mappings.lua
@@ -5,8 +5,8 @@ M.general = {
   n = {
     ["<leader>te"] = { "<CMD>Translate EN<CR>", "Translate selection to englisch", { nowait = true}},
     ["<leader>tg"] = { "<CMD>Translate DE<CR>", "Translate selection to german", { nowait = true}},
-    ["<leader>gg"] = { "<CMD>LazyGitToggle<CR>", "Open LazyGit", { nowait = true }},
-    ["<leader>dd"] = { "<CMD>LazyDockerToggle<CR>", "Open LazyDocker", { nowait = true }},
+    ["<leader>gg"] = { "<CMD>LazyGitToggle<CR>", "Open LazyGit"},
+    ["<leader>dd"] = { "<CMD>LazyDockerToggle<CR>", "Open LazyDocker"},
   },
 }
 
