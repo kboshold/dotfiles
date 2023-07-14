1dc9be7acf94a13c368d582f44d04a1c02777414
Kevin Boshold <github@proxy.boshol.de>
2023-07-14 18:21:23 +0200

    Update lspconfig.lua

--- dot_config/nvim.custom/configs/lspconfig.lua
+++ dot_config/nvim.custom/configs/lspconfig.lua
@@ -2,7 +2,13 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "angularls", "tsserver", "docker_compose_language_service", "kotlin_language_server"}
+local servers = { "html", "cssls", "clangd", "angularls", "tsserver", "docker_compose_language_service"}
+
+  lspconfig['kotlin_language_server'].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "kotlin", "kt" }
+  }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
