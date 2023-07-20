d8e4733f72af0c551bfa3c58fb6f92636dd5130d
Kevin Boshold <github@proxy.boshol.de>
2023-07-20 10:19:08 +0200

    Update options.lua

--- dot_config/nvim.custom/options.lua
+++ dot_config/nvim.custom/options.lua
@@ -28,3 +28,9 @@ vim.opt.listchars = {
   tab = ' '
 }
 vim.opt.list = true
+
+vim.opt.smartindent = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
