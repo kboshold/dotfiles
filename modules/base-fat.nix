0276d3f9a3af905d4625067056cd707538d380b4
Kevin Boshold <github@proxy.boshol.de>
2025-02-12 16:36:16 +0100

    Update base-fat.nix

--- modules/base-fat.nix
+++ modules/base-fat.nix
@@ -18,16 +18,16 @@
     nerd-fonts.jetbrains-mono
   ];
   home.file = {
-    "~/.config/eza".source = ../config/eza;
-    "~/.config/fish".source = ../config/fish;
-    "~/.config/atuin".source = ../config/atuin;
-    "~/.config/mise".source = ../config/mise;
-    "~/.config/nushell".source = ../config/nushell;
-    "~/.config/oh-my-posh".source = ../config/oh-my-posh;
-    "~/.config/starship".source = ../config/starship;
-    "~/.config/zsh".source = ../config/zsh;
-    "~/.config/zellij".source = ../config/zellij;
-    "~/.config/.fdignore".source = ../config/fdignore;
-    "~/.config/.rgignore".source = ../config/rgignore;
+    ".config/eza".source = ../config/eza;
+    ".config/fish".source = ../config/fish;
+    ".config/atuin".source = ../config/atuin;
+    ".config/mise".source = ../config/mise;
+    ".config/nushell".source = ../config/nushell;
+    ".config/oh-my-posh".source = ../config/oh-my-posh;
+    ".config/starship".source = ../config/starship;
+    ".config/zsh".source = ../config/zsh;
+    ".config/zellij".source = ../config/zellij;
+    ".config/.fdignore".source = ../config/fdignore;
+    ".config/.rgignore".source = ../config/rgignore;
   };
 }
