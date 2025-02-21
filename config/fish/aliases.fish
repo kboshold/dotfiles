0d1f2121df46fca4d963bb98746ed5aeb10d4d07
Kevin Boshold <github@proxy.boshol.de>
2025-02-21 16:45:52 +0000

    Fixed formatting

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -9,38 +9,38 @@ alias dir ls
 set dots ""
 set levels ""
 for i in (seq 1 9)
-    set dots ".$dots"
-    set levels "../$levels"
+	set dots ".$dots"
+	set levels "../$levels"
 
-    alias ".$i" "cd $levels"
-    alias ".$dots" "cd $levels"
-    alias "cd.$dots" "cd $levels"
-    alias "cd$i" "cd $levels"
+	alias ".$i" "cd $levels"
+	alias ".$dots" "cd $levels"
+	alias "cd.$dots" "cd $levels"
+	alias "cd$i" "cd $levels"
 end
 
 alias cg 'git rev-parse 2>/dev/null && cd "$(git rev-parse --show-cdup)"'
 alias cdg cg
 
 if command -sq fd
-    function cdf 
-				if command -sq eza
-					set -l CDF_FZF_PREVIEW_COMMAND "eza --long --color=always --all --icons --git  --hyperlink -g {}"
-				else
-					set -l CDF_FZF_PREVIEW_COMMAND "ls -la"
-				end
+	function cdf 
+		if command -sq eza
+			set -l CDF_FZF_PREVIEW_COMMAND "eza --long --color=always --all --icons --git  --hyperlink -g {}"
+		else
+			set -l CDF_FZF_PREVIEW_COMMAND "ls -la"
+		end
 
-        set -l directory (fd -0 --type d --hidden | fzf --read0 --preview "$CDF_FZF_PREVIEW_COMMAND")
-        if test -n "$directory"
-            cd $directory
-        end
-    end
+		set -l directory (fd -0 --type d --hidden | fzf --read0 --preview "$CDF_FZF_PREVIEW_COMMAND")
+		if test -n "$directory"
+			cd $directory
+		end
+	end
 
-    alias cf cdf
+	alias cf cdf
 end 
 
 # Override ls with eza
 if command -sq eza
-    alias ls "eza --git --icons"
+	alias ls "eza --git --icons"
 end
 
 # Use vim as default
@@ -58,14 +58,14 @@ set -gx VISUAL vim
 
 # If possible use neovim
 if command -sq nvim
-    alias vim "nvim"
+	alias vim "nvim"
 
-    set -gx EDITOR nvim
-    set -gx GIT_EDITOR nvim
-    set -gx VISUAL nvim
+	set -gx EDITOR nvim
+	set -gx GIT_EDITOR nvim
+	set -gx VISUAL nvim
 end
 
 # If possible use bottom
 if command -sq btm
-    alias top "btm"
+	alias top "btm"
 end
