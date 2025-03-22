e90061816ea0db8455ec9d6610628a81e1eaa570
Kevin Boshold <github@proxy.boshol.de>
2025-03-22 18:34:55 +0100

    refactor: reorganize flake structure and integrate sops-nix
    - Replace custom secrets input with sops-nix for secret management
    - Restructure file organization with a clearer systems hierarchy
    - Add NixOS configurations for laptop, desktop, and work systems
    - Rename and reorganize home-manager configurations
    - Add devShell with sops-related tools
    - Extend system support to multiple architectures

--- flake.nix
+++ flake.nix
@@ -3,61 +3,105 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
 		home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		secrets = {
-			url = "path:./secrets";
-			flake = false;
+		flake-utils.url = "github:numtide/flake-utils";
+
+		sops-nix = {
+			url = "github:Mic92/sops-nix";
+			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
 
-	outputs = { self, nixpkgs, secrets ? null, home-manager, ... }:
+	outputs = { self, nixpkgs, home-manager, flake-utils, sops-nix, ... }@inputs:
 		let
-			# Use the system from the environment or default to x86_64-linux
 			system = builtins.currentSystem or "x86_64-linux";
 			pkgs = import nixpkgs { inherit system; };
 			data = builtins.fromTOML (builtins.readFile ./data.toml);
+
+			supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+			forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 		in {
+			nixosConfigurations = {
+				laptop = nixpkgs.lib.nixosSystem {
+					system = "x86_64-linux";
+					modules = [
+						./systems/home/laptop.nix
+						home-manager.nixosModules.home-manager
+						sops-nix.nixosModules.sops
+						{
+							_module.args = { inherit data inputs; };
+						}
+					];
+				};
+
+				desktop = nixpkgs.lib.nixosSystem {
+					system = "x86_64-linux";
+					modules = [
+						./systems/home/desktop.nix
+						home-manager.nixosModules.home-manager
+						sops-nix.nixosModules.sops
+						{
+							_module.args = { inherit data inputs; };
+						}
+					];
+				};
+
+				work = nixpkgs.lib.nixosSystem {
+					system = "x86_64-linux";
+					modules = [
+						./systems/work/default.nix
+						home-manager.nixosModules.home-manager
+						sops-nix.nixosModules.sops
+						{
+							_module.args = { inherit data inputs; };
+						}
+					];
+				};
+			};
 
 			homeConfigurations = {
-				home = home-manager.lib.homeManagerConfiguration {
+				wsl = home-manager.lib.homeManagerConfiguration {
 					inherit pkgs;
-					modules = [ ./modules/home.nix ];
-					extraSpecialArgs = { inherit data secrets; };
+					modules = [ ./systems/home/wsl.nix ];
+					extraSpecialArgs = { inherit data; };
 				};
 
-				home-wsl = home-manager.lib.homeManagerConfiguration {
+				server-light = home-manager.lib.homeManagerConfiguration {
 					inherit pkgs;
-					modules = [ ./modules/home-wsl.nix ];
-					extraSpecialArgs = { inherit data secrets; };
-				};
-
-				work = home-manager.lib.homeManagerConfiguration {
-					inherit pkgs;
-					modules = [ ./modules/work.nix ];
-					extraSpecialArgs = { inherit data secrets; };
-				};
-
-				server = home-manager.lib.homeManagerConfiguration {
-					inherit pkgs;
-					modules = [ ./modules/server.nix ];
-					extraSpecialArgs = { inherit data secrets; };
+					modules = [ ./systems/server/light.nix ];
+					extraSpecialArgs = { inherit data; };
 				};
 
 				server-fat = home-manager.lib.homeManagerConfiguration {
 					inherit pkgs;
-					modules = [ ./modules/server-fat.nix ];
-					extraSpecialArgs = { inherit data secrets; };
+					modules = [ ./systems/server/server-fat.nix ];
+					extraSpecialArgs = { inherit data; };
 				};
 
-				codespaces = home-manager.lib.homeManagerConfiguration {
+				devcontainer = home-manager.lib.homeManagerConfiguration {
 					inherit pkgs;
-					modules = [ ./modules/codespaces.nix ];
-					extraSpecialArgs = { inherit data secrets; };
+					modules = [ ./systems/server/devcontainer.nix ];
+					extraSpecialArgs = { inherit data; };
 				};
 			};
+
+			devShells = forAllSystems (system:
+				let pkgs = nixpkgs.legacyPackages.${system};
+				in {
+					default = pkgs.mkShell {
+						buildInputs = with pkgs; [
+							git
+							sops
+							ssh-to-age
+							age
+						];
+					};
+				}
+			);
 		};
 }
